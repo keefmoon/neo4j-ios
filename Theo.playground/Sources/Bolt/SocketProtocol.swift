92802a140ce1eb97b2e000e47e2b72cfae2df427
Keith Moon <keef@keefmoon.com>
2018-03-10 23:00:54 +0000

    iOS Playground working

---
+++ Theo.playground/Sources/Bolt/SocketProtocol.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public protocol SocketProtocol {
+
+    func connect(timeout: Int) throws
+    func send(bytes: [Byte]) throws
+    func receive(expectedNumberOfBytes: Int32) throws -> [Byte]
+    func disconnect()
+}
